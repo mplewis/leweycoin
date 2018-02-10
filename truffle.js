09eefc4f21d2b220fb422a1f7e0b8fd458a8c1cb
Matt Lewis <matt@mplewis.com>
2018-02-09 19:21:04 -0700

    config: live/rinkeby networks

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*", // Match any network id
+      network_id: '*'
     },
-  },
-};
+    rinkeby: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '4'
+    },
+    live: {
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '1'
+    }
+  }
+}
